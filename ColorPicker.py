4a4fdb810bcaf1e8b6c68a93dba660a9084844f3
adelppi <adelppi.baig@gmail.com>
2023-01-09 00:12:35 +0900

    commit

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,17 +1,32 @@
 import pyautogui as pg
 import PySimpleGUI as sg
+import keyboard
+import time
+
+def createButton(buttonColor):
+    element = [sg.B("", size=(2,1), button_color=buttonColor, border_width = 1, key = "buttonPressed")]
+    return element
+
+def RGB2HEX(color):
+    return "#%02x%02x%02x" % color
 
 sg.theme('Default 1')
-layout = [[sg.Text('RGB = ', key = "-ACT-")]]
 
-window = sg.Window('sample', layout, size = (300, 200))
+layout = [[sg.Column([createButton("#000000")], key = "createButton")],
+          [sg.T("RGB = ", key = "updateColor")]]
+window = sg.Window('ColorPicker', layout, return_keyboard_events=True, size = (300, 200))
 
 while True:
     event, values = window.read(timeout = 50)
     if event == sg.WIN_CLOSED:
         break
+
+    if keyboard.is_pressed("enter"):
+        window.extend_layout(window["createButton"], [createButton(RGB2HEX(color))])
+        pg.keyUp("enter")
+
     x, y = pg.position()
     color = pg.pixel(x, y)
-    window["-ACT-"].update(color)
+    window["updateColor"].update("RGB = " + str(color))
 
 window.close()
