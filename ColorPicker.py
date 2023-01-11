06b08f34c3e9bc154c33dae776ac0b85c331780c
adelppi <adelppi.baig@gmail.com>
2023-01-11 23:50:24 +0900

    commit

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,10 +1,11 @@
 import pyautogui as pg
 import PySimpleGUI as sg
+import pyperclip
 import keyboard
 import time
 
 def createButton(buttonColor):
-    element = [sg.B("", size=(2,1), button_color=buttonColor, border_width = 1, key = "buttonPressed")]
+    element = [sg.B("", size=(2,1), button_color=buttonColor, border_width = 1, key = "button" + buttonColor)]
     return element
 
 def RGB2HEX(color):
@@ -12,8 +13,7 @@ def RGB2HEX(color):
 
 sg.theme('Default 1')
 
-layout = [[sg.Column([createButton("#000000")], key = "createButton")],
-          [sg.T("RGB = ", key = "updateColor")]]
+layout = [[sg.Column([createButton("#000000")], key = "createButton")],[sg.T("RGB = ", key = "updateColor")]]
 window = sg.Window('ColorPicker', layout, return_keyboard_events=True, size = (300, 200))
 
 while True:
@@ -24,6 +24,10 @@ while True:
     if keyboard.is_pressed("enter"):
         window.extend_layout(window["createButton"], [createButton(RGB2HEX(color))])
         pg.keyUp("enter")
+
+    if "button#" in event:
+        buttonInfo = event.replace("button#", "")
+        pyperclip.copy(str(buttonInfo))
 
     x, y = pg.position()
     color = pg.pixel(x, y)
